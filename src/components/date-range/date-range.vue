7b7fe29f055b371b561739bec0d101a39e6426bb
AmyFoxFN <funan_amy@foxmail.com>
2018-03-19 16:03:08 +0800

    <add>(date-range)

---
+++ src/components/date-range/date-range.vue
@@ -0,0 +1,40 @@
+<template>
+  <cube-date-picker
+    :min="min"
+    :max="max"
+    v-model="start"></cube-date-picker>
+  <cube-date-picker></cube-date-picker>
+</template>
+
+<script>
+const COMPONENT_NAME = 'cube-date-range'
+export default {
+  name: COMPONENT_NAME,
+  props: {
+    min: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    max: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    start: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    end: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
